045ac320de1680dcc32ca1e30d065bf28f226d11
Jason Culbertson <jason.culbertson@linktr.ee>
2024-11-23 21:11:53 -0800

    style change to apple

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,42 +4,62 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-        serif: ['Merriweather', 'Georgia', 'serif'],
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'SF Pro Text',
+          'SF Pro Icons',
+          'Helvetica Neue',
+          'Helvetica',
+          'Arial',
+          'sans-serif'
+        ],
+        serif: ['New York', 'Georgia', 'serif'],
       },
       typography: {
         DEFAULT: {
           css: {
-            '--tw-prose-body': 'rgb(41, 41, 41)',
-            '--tw-prose-headings': 'rgb(41, 41, 41)',
-            maxWidth: '65ch',
-            fontSize: '1.125rem',
-            lineHeight: '1.8',
-            color: 'rgb(41, 41, 41)',
-            fontFamily: 'Merriweather, Georgia, serif',
+            '--tw-prose-body': '#1d1d1f',
+            '--tw-prose-headings': '#1d1d1f',
+            '--tw-prose-links': '#0066cc',
+            '--tw-prose-bold': '#1d1d1f',
+            '--tw-prose-bullets': '#86868b',
+            maxWidth: '680px',
+            fontSize: '19px',
+            lineHeight: '1.47059',
+            color: '#1d1d1f',
+            fontFamily: '-apple-system, BlinkMacSystemFont, SF Pro Text, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif',
             h1: {
-              fontFamily: 'Inter, system-ui, sans-serif',
-              fontWeight: '800',
-              fontSize: '2.5rem',
-              marginBottom: '2rem',
-              lineHeight: '1.2'
+              fontFamily: '-apple-system, BlinkMacSystemFont, SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif',
+              fontWeight: '600',
+              fontSize: '48px',
+              letterSpacing: '-0.003em',
+              marginTop: '0',
+              marginBottom: '1.5rem',
+              lineHeight: '1.08349'
             },
             h2: {
-              fontFamily: 'Inter, system-ui, sans-serif',
-              fontWeight: '700',
-              fontSize: '1.875rem',
+              fontFamily: '-apple-system, BlinkMacSystemFont, SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif',
+              fontWeight: '600',
+              fontSize: '28px',
+              letterSpacing: '.007em',
               marginTop: '2.5rem',
               marginBottom: '1.25rem',
-              lineHeight: '1.3'
+              lineHeight: '1.20849'
             },
             h3: {
-              fontFamily: 'Inter, system-ui, sans-serif',
+              fontFamily: '-apple-system, BlinkMacSystemFont, SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif',
               fontWeight: '600',
-              fontSize: '1.5rem'
+              fontSize: '24px',
+              letterSpacing: '.009em',
+              marginTop: '2rem',
+              marginBottom: '1rem',
+              lineHeight: '1.20849'
             },
             p: {
-              marginTop: '1.5rem',
-              marginBottom: '1.5rem'
+              marginTop: '0',
+              marginBottom: '1.4em',
+              lineHeight: '1.47059'
             },
             'code::before': {
               content: '""'
@@ -48,11 +68,43 @@ export default {
               content: '""'
             },
             code: {
-              color: 'rgb(41, 41, 41)',
-              backgroundColor: 'rgb(243, 244, 246)',
+              color: '#1d1d1f',
+              backgroundColor: '#f5f5f7',
               padding: '0.2em 0.4em',
-              borderRadius: '0.25rem',
-              fontSize: '0.875em'
+              borderRadius: '4px',
+              fontSize: '0.9em',
+              fontFamily: 'SF Mono, Monaco, Consolas, monospace'
+            },
+            ul: {
+              marginTop: '0.8em',
+              marginBottom: '0.8em',
+            },
+            li: {
+              marginTop: '0.5em',
+              marginBottom: '0.5em',
+              lineHeight: '1.47059',
+            },
+            'ul > li': {
+              paddingLeft: '1.5em',
+            },
+            'ul > li::before': {
+              width: '0.375em',
+              height: '0.375em',
+              top: '0.6875em',
+              left: '0.25em',
+              backgroundColor: '#86868b',
+              borderRadius: '50%'
+            },
+            strong: {
+              fontWeight: '600',
+              color: '#1d1d1f'
+            },
+            a: {
+              color: '#0066cc',
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline'
+              }
             }
           }
         }
