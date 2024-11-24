073f90d692d4bc315bce3218b33a6505eef304b6
Jason Culbertson <jason.culbertson@linktr.ee>
2024-11-23 20:58:52 -0800

    updated from bolt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), react()],
   markdown: {
     shikiConfig: {
       theme: 'github-light'
