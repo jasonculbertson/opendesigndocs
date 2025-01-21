ce6420a34656033a5d8b155fea68c89fd99c9723
Jason Culbertson <jason.culbertson@linktr.ee>
2025-01-20 20:27:28 -0800

    fix 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-    includeFiles: ['.vercel/output/config.json'],
-    functionPerRoute: false,
-    maxDuration: 60
-  }),
+  output: 'static',
   integrations: [
     mdx({
       components: {
