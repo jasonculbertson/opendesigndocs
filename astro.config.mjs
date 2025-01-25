cc46add45aa07b654ccf1c0f3e83f4c7d1721a62
Jason Culbertson <jason.culbertson@linktr.ee>
2025-01-24 16:55:29 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     mdx({
       components: {
