3abb3ed75d135c5c0456004acf7dc1c0d95a525a
Jason Culbertson <jason.culbertson@linktr.ee>
2025-01-24 16:54:08 -0800

    Update subscribe.ts

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -3,7 +3,7 @@ import { supabaseAdmin } from '../../lib/supabase';
 
 export const prerender = false;
 
-export async function POST({ request }) {
+export const post: APIRoute = async ({ request }) => {
   try {
     const body = await request.json();
     const { email, marketingOptIn } = body;
