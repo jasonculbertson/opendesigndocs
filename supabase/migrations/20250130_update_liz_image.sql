a80e89076134701311154e67019794411f791f43
jasonculbertson <jculbertson@gmail.com>
2025-09-09 09:54:26 -0700

    Fix Liz image file extension from .jpeg to .jpg
    - Updated invite page configuration to use correct filename
    - Updated database migration to match actual file extension
    - Image file was saved as liz.jpg but config was looking for liz.jpeg

--- supabase/migrations/20250130_update_liz_image.sql
+++ supabase/migrations/20250130_update_liz_image.sql
@@ -1,4 +1,4 @@
 -- Update Liz Luce's image to use local image file (download from LinkedIn)
 UPDATE recruiters 
-SET image = '/images/liz.jpeg' 
+SET image = '/images/liz.jpg' 
 WHERE slug = 'liz-luce';
